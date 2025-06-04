eedc87ff8a49ae22efe3b8c094283bf953042572
HellenNyandaraAchieng <hachieng116@gmail.com>
2025-06-04 21:24:06 +0300

    Sidebar updates

---
+++ bookingme/app/bookings.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function bookings() {
+  return (
+    <div>
+      <h1>Room Allocation</h1>
+    </div>
+  )
+}
---
+++ bookingme/app/page.tsx
@@ -0,0 +1,10 @@
+import { AppSidebar } from "./layout/sidebarmenu";
+
+
+export default function HomePage() {
+  return (
+    <div className="p-4">
+      <AppSidebar />
+    </div>
+  );
+}
